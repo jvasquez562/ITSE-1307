64476b4d87011663fda3cb3948826dc83dec7a9d
jvasquez562 <47163802+jvasquez562@users.noreply.github.com>
2019-04-02 21:11:11 -0500

    Update Prime_Numbers.cpp

--- Prime_Numbers/Prime_Numbers.cpp
+++ Prime_Numbers/Prime_Numbers.cpp
@@ -1,55 +1,52 @@
-// Prime_Numbers.cpp : Defines the entry point for the console application.
 //Jeremy Vasquez
 //ITSE 1302
 //4-2-19
-
-#include "stdafx.h"
-#include <iostream>
-
-using namespace std;
-
-
-bool isPrime(int intValue) {
-   int intPrimeDivisionCount = 0;
-
-   if (intValue == 0) {
-      return false;
-      //cout << "Not Prime";
-   }
-   else {
-      for (int intPrimeCheck = 2;
-         intPrimeCheck < intValue;
-         intPrimeCheck++) {
-
-         if (intValue % intPrimeCheck == 0) {
-            intPrimeDivisionCount++;
-         }
-      }
-   }
-
-   if (intPrimeDivisionCount >= 1) {
-      return false;
-      //cout << "Not a prime!" << endl;
-   }
-
-   return true;
-}
-
-int main()
-{
-   int intNumber = 0;
-
-   for (int intRange = 1; intRange <= 10000; intRange++) {
-      intNumber = intRange;
-
-      if (isPrime(intNumber)) {
-         cout << ", " << intNumber;
-      }
-   }
-
-
-
-
-   return 0;
-}
-
+	
+	#include "pch.h"
+	#include <iostream>
+	using namespace std;
+	
+	int main()
+	{
+		int intNumber = 0;
+		int intPrimeDivisionCount = 0;
+		bool boolIsPrime = true;
+	
+		for (int intRange = 1; intRange <= 10000; intRange++) {
+			intNumber = intRange;
+	
+			intPrimeDivisionCount = 0;
+			boolIsPrime = true;
+			//Start with the guess it is prime then set to false if not true
+			if (intNumber == 0) {
+				boolIsPrime = false;
+				//cout << "Not Prime";
+			}
+			else {
+				for (int intPrimeCheck = 2;
+					intPrimeCheck < intNumber;
+					intPrimeCheck++) {
+	
+					if (intNumber % intPrimeCheck == 0) {
+						intPrimeDivisionCount++;
+					}
+	
+					//cerr << "N: " << intNumber <<
+					//	", PC: " << intPrimeCheck <<
+					//	", PDC: " << intPrimeDivisionCount << endl;
+				}
+			}
+	
+			if (intPrimeDivisionCount >= 1) {
+				boolIsPrime = false;
+				//cout << "Not a prime!" << endl;
+			}
+	
+			if (boolIsPrime) {
+				cout << ", " << intNumber;
+			}
+		}
+	
+	}
+	
+	
