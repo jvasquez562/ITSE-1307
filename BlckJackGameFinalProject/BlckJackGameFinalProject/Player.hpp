bc6a75017e8293dab8e10ec444872a8e384983f0
jvasquez562 <47163802+jvasquez562@users.noreply.github.com>
2019-05-09 19:22:17 -0500

    Update Player.hpp

--- BlckJackGameFinalProject/BlckJackGameFinalProject/Player.hpp
+++ BlckJackGameFinalProject/BlckJackGameFinalProject/Player.hpp
@@ -6,9 +6,45 @@
 //  Copyright © 2019 Jeremy Vasquez. All rights reserved.
 //
 
-#ifndef Player_hpp
+
 #define Player_hpp
 
 #include <stdio.h>
+#pragma once
+#include <string>
+#include <vector>
+#include "Card.hpp"
 
-#endif /* Player_hpp */
+class Player
+{
+public:
+    Player();
+    ~Player();
+private:
+    int intPlayerNumber;
+    int intMoney;
+    int intBet;
+    std::string strName;
+    std::vector<Card> vtrPlayerHand;
+    void setPlayerNumber();
+public:
+    std::string inputPlayerName();
+    void setPlayerName(std::string strpName);
+    int getHandPointValue();
+    void printHand();
+    std::string toString();
+    std::string getHandString();
+    std::string getPlayerName();
+    bool addCard(Card objpCard);
+    int getNumberCards();
+    void clearHand();
+    
+    void setMoney(int intpMoney);
+    int getMoney();
+    void setBet(int intpBet);
+    int getBet();
+    int inputPlayerBet();
+    
+    
+    
+};
